acd116a1d5e2c3892ec27e4b5261a5836a49f40c
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-08-06 01:03:12 +0300

    feat: add Promo and AboutProject sections

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import './Main.css';
+import Promo from "./Promo/Promo";
+import AboutProject from "./AboutProject/AboutProject";
+
+function Main() {
+    return (
+      <main className="content">
+        <Promo />
+        <AboutProject />
+      </main>
+    );
+  }
+
+export default Main;
---
+++ src/components/Main/Promo/Promo.js
@@ -0,0 +1,25 @@
+import './Promo.css';
+import promoLogo from '../../../images/promo-logo.svg';
+
+function Promo() {
+  return (
+    <section className="promo">
+      <div className="promo__group">
+        <div className="promo__info">
+          <h1 className="promo__title">Учебный проект студента факультета Веб-&zwj;разработки.</h1>
+          <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+          <button 
+            className="promo__learn-more-button"
+            type="button"
+            aria-label="Узнать больше"
+            >Узнать больше
+          </button>   
+        </div>
+        <img className="promo__logo" alt='Планета Земля, нарисованная из множества надписей web' src={promoLogo}/>
+      </div>      
+        
+    </section>
+  );
+}
+
+export default Promo;
